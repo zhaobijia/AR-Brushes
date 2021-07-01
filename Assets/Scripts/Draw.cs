33af09e9b912978faed262ac0be7b9bca63a096c
bijia <zhaobijia@gmail.com>
2021-07-01 00:46:40 -0400

    Add touch and draw

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 
 public class Draw : MonoBehaviour
@@ -13,6 +14,16 @@ public class Draw : MonoBehaviour
     LineRenderer line;
     HashSet<Vector3> points = new HashSet<Vector3>();
     private void Update()
+    {
+#if UNITY_EDITOR
+        DrawOnMouse();
+#endif
+#if UNITY_ANDROID || UNITY_IPHONE
+        DrawOnTouch();
+#endif
+    }
+
+    private void DrawOnMouse()
     {
         if (Input.GetMouseButton(0))
         {
@@ -26,6 +37,28 @@ public class Draw : MonoBehaviour
             else
             {
                 InitializeLineRenderer(mousePosition);
+            }
+        }
+    }
+
+    private void DrawOnTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = arCam.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, distanceFromCamera));
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    InitializeLineRenderer(touchPosition);
+                    break;
+                case TouchPhase.Moved:
+                    UpdateLineRenderer(touchPosition);
+                    break;
+                case TouchPhase.Ended:
+                    break;
+
             }
         }
     }
